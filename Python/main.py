4f1ad137000c37b87f9799a20e957ac7dd2772e2
Akul Arora <akularora@gmail.com>
2020-05-05 21:55:55 -0700

    Have a rudimentary (working) build

--- Python/main.py
+++ Python/main.py
@@ -14,11 +14,13 @@ import logging
 
 # Constants
 REPO_NAME = "My-All-Weather-Strategy"
+DATABASE_ADDR = "influxdb-grafana"
 DATABASE_NAME = "All_Weather"
 STRATEGY = "../strategy.json"
 LOG = "DONOTPUSH/errors.log"
-logger = None
 
+# Global objects
+client = None
 
 
 def main(argv):
@@ -32,7 +34,7 @@ def main(argv):
     with open(STRATEGY, 'r') as f:
         strategy_json = json.loads(f.read())
         portfolio_json = strategy_json['Portfolio']
-        strategy_name = strategy_json["Name"]
+        strategy_name = strategy_json['Name']
         band_threshold = strategy_json['Percent Band Threshold']
         
     # Get all valid tickers
@@ -47,62 +49,59 @@ def main(argv):
         if ticker != '':
             write_stock_price(asset['Name'], ticker, ticker_performance[ticker])
 
-    print("done")
-        
-
-
 def get_stock_changes(tickers):
     """
     Calculate price changes on all tickers
     """
     dict = {}
     for ticker in tickers:
-        today_price = get_price_today(ticker)
+        value = get_price_today(ticker)
         try:
-            prev_price = get_price_prev(ticker)
-            change_price = (today_price - prev_price) / prev_price * 100
-        except Exception as e: # If I haven't already put in the data. Bad practice I know.
-            change_price = 0
-            logger.error("Could not pull value for ticker " + ticker + "from database")
-        info = {"value": today_price, "Percent Change": change_price}
+            # prev_price = get_price_prev(ticker)
+            prev_price = 0
+            percent_change = (value - prev_price) / prev_price * 100
+        except Exception: # If I haven't already put in the data. Bad practice I know.
+            percent_change = 0
+            logging.error("Could not pull value for ticker " + ticker + " from database")
+        info = {"value": value, "Percent Change": percent_change}
         dict[ticker] = info
 
     return dict
 
-    def get_price_prev(ticker):
-        """
-        Pulls data from InfluxDB using list of tickers.
-        Returns back previous close for ticker.
-        """
-        search = "SELECT value FROM price WHERE ticker = " + ticker \
-            + "GROUP BY * ORDER BY ASC LIMIT 1"
-        client = InfluxDBClient('localhost', 8086, 'root', 'root', DATABASE_NAME)
-        price = client.query(search)
+def get_price_prev(ticker):
+    """
+    Pulls data from InfluxDB using list of tickers.
+    Returns back previous close for ticker.
+    """
+    search = "SELECT value FROM price WHERE ticker = " + ticker \
+        + " GROUP BY * ORDER BY ASC LIMIT 1"
+    print(search)
+    result = client.query(search, database=DATABASE_NAME)
 
-        return price
+    price = result.items()
+    return price
 
-    def get_price_today(ticker):
-        """
-        Pulls data from Alphavantage using ticker.
-        Returns back close for today (latest value).
-        """
-        ts = TimeSeries(api_keys.AV_KEY)
-        attempts = 1
-        while attempts <= 2:
-            try:
-                data = ts.get_quote_endpoint(symbol=ticker)
-                price = float(data[0]['05. price'])
-                return price
-            except ValueError:
-                sleep(61)
-                attempts += 1
+def get_price_today(ticker):
+    """
+    Pulls data from Alphavantage using ticker.
+    Returns back close for today (latest value).
+    """
+    ts = TimeSeries(api_keys.AV_KEY)
+    attempts = 1
+    while attempts <= 2:
+        try:
+            data = ts.get_quote_endpoint(symbol=ticker)
+            price = float(data[0]['05. price'])
+            return price
+        except ValueError:
+            sleep(61)
+            attempts += 1
 
 def get_current_portfolio():
     '''
     search = "SELECT value FROM balance WHERE ticker = " + ticker \
         + "GROUP BY * ORDER BY ASC LIMIT 1"
-    client = InfluxDBClient('localhost', 8086, 'root', 'root', DATABASE_NAME)
-    latest = client.query(search)
+    latest = client.query(search, DATABASE_NAME)
     '''
 
 def update_portfolio(curr, ticker_performance):
@@ -148,12 +147,10 @@ def write_point(point):
     Documentation used: http://influxdb-python.readthedocs.io/en/latest/include-readme.html#documentation
     Note: This is code I have copied from one of my other repositories (Caltran_traffic).
     """
-    try:
-        client = InfluxDBClient('localhost', 8086, 'root', 'root', DATABASE_NAME)
-        client.create_database(DATABASE_NAME)
-        client.write_points(data_point, time_precision = 'h')
-    except:
-        logging.error("Could not write data for {} due to error with InfluxDB database".format(point))
+    # try:
+    client.write_points(point, database=DATABASE_NAME, time_precision = 'h')
+    # except:
+        # logging.error("Could not write data for {} due to error with InfluxDB database".format(point))
 
 def update_contents():
     g = Github(api_keys.GH_KEY)
@@ -161,9 +158,13 @@ def update_contents():
 
 
 if __name__ == "__main__":
-    try:
-        logging.basicConfig(filename=LOG, level=logging.DEBUG,
-            format='%(asctime)s %(levelname)s %(name)s %(message)s')
-        main(sys.argv[1:])
-    except Exception as e:
-        logging.error(e)
+    # try:
+    logging.basicConfig(filename=LOG, level=logging.DEBUG,
+        format='%(asctime)s %(levelname)s %(name)s %(message)s')
+
+    client = InfluxDBClient(DATABASE_ADDR, 8086, DATABASE_NAME)
+    client.create_database(DATABASE_NAME)
+
+    main(sys.argv[1:])
+# except Exception as e:
+# logging.error(e)
